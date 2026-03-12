bebd1ec2cb1cbf7ec40318694e760e92932e2fd9
Minsu Lee <amond@amond.net>
2026-03-12 16:18:03 +0900

    feat(eslint-config): add package-json recommended export
    - Add eslint-plugin-package-json dependency
    - Export recommended-publishable and stylistic configs via ./package-json subpath
    - Add CI workflow with ESLint caching
    - Enable --cache flag on lint scripts

---
+++ packages/eslint-config/src/package-json.ts
@@ -0,0 +1,6 @@
+import packageJson from 'eslint-plugin-package-json'
+
+export const recommended = packageJson.configs['recommended-publishable']
+export const stylistic = packageJson.configs.stylistic
+
+export default recommended
